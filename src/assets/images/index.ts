c5f5eb0d754222967507b6974a1752caa320ef39
Alessandro Baldassarre <alessandro.baldassarre.dev@gmail.com>
2023-05-10 15:36:05 +0200

    Add custom-site icons

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -5,6 +5,12 @@ import meetings from "@/assets/images/meetingrooms.svg";
 import location from "@/assets/images/location.svg";
 import hero_img from "@/assets/images/hero.png";
 import logo from "@/assets/images/logo.png";
+import expert from "@/assets/images/expert_icon.svg";
+import membership from "@/assets/images/membership_icon.svg";
+import solution from "@/assets/images/solution_icon.svg";
+import virtual from "@/assets/images/virtual_icon.svg";
+import workspace from "@/assets/images/workspace_icon.svg";
+import app_icon from "@/assets/images/app_icons.svg";
 
 export {
   office,
@@ -14,4 +20,10 @@ export {
   location,
   hero_img,
   logo,
+  expert,
+  membership,
+  solution,
+  virtual,
+  workspace,
+  app_icon,
 };
